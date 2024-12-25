2fc11e7f619fd1ab7b26010acc69c81ae9f23768
giorgi-2001 <gio19900504@gmail.com>
2024-12-25 13:03:02 +0400

    pagination feature finished

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -7,7 +7,8 @@ app_name = "blog"
 
 urlpatterns = [
     path("", views.PostList.as_view(), name="post-list"),
-    path("posts/create", views.PostCreate.as_view(), name="post-create"),
+    path("posts/create/", views.PostCreate.as_view(), name="post-create"),
+    path("posts/user/<str:author>/", views.UserPosts.as_view(), name="user-posts"),
     path("posts/<slug:slug>/", views.PostDetail.as_view(), name="post-detail"),
     path("posts/<slug:slug>/update/", views.PostUpdate.as_view(), name="post-update"),
     path("posts/<slug:slug>/delete/", views.PostDelete.as_view(), name="post-delete"),
