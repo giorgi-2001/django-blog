23d8236018f9cf7d030cda9ff0762a5675d0400e
giorgi-2001 <gio19900504@gmail.com>
2024-12-25 23:33:55 +0400

    prod env

--- django_project/project/settings.py
+++ django_project/project/settings.py
@@ -76,7 +76,7 @@ TEMPLATES = [
 ]
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+
 ]
 
 WSGI_APPLICATION = 'project.wsgi.application'
@@ -87,8 +87,12 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv("POSTGRES_DB"),
+        'USER': os.getenv("POSTGRES_USER"),
+        'PASSWORD': os.getenv("POSTGRES_PASSWORD"),
+        'HOST': 'postgres',
+        'PORT': '5432',
     }
 }
 
@@ -128,7 +132,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = BASE_DIR / "static"
 
 MEDIA_ROOT = BASE_DIR / "media"
 MEDIA_URL = "/media/"
